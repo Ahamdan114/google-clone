93c4f149ef0b89edb56ff1f0ae2863a3ce1b408c
Ahmed-Iulian Hamdan <Ahamdan114>
2023-08-14 18:29:24 +0300

    Added the pages folder for splitting the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ function App() {
     return (
         <div className="App">
             <h1>Google Clone incoming</h1>
+
+            {/* HomePage ( The one with the search on ) */}
+            {/* SearchPage ( The results page ) */}
         </div>
     );
-    
 }
 
 export default App;
